c042e0a3918b7df1ccc17341d54870a3f4f3bb46
Jesús García <jesus.garciav@me.com>
2018-10-05 00:04:12 -0500

    PHPUnit tests

--- tests/Unit/LinioTest.php
+++ tests/Unit/LinioTest.php
@@ -6,15 +6,73 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Linio;
+
 class LinioTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+  use RefreshDatabase;
+
+  public function setUp()
+  {
+    parent::setUp();
+
+    foreach (range(1, 100) as $number)
     {
-        $this->assertTrue(true);
+      factory(Linio::class)->create(['number' => $number]);
     }
+  }
+
+  /** @tests */
+  public function testIfThirtyIsMultipleOfThree()
+  {
+    $linio = Linio::find(30);
+
+    $this->assertEquals($linio->number, 30);
+    $this->assertTrue($linio->isMultipleOfThree());
+  }
+
+  /** @tests */
+  public function testIfFiftyIsMultipleOfFive()
+  {
+    $linio = Linio::find(50);
+
+    $this->assertEquals($linio->number, 50);
+    $this->assertTrue($linio->isMultipleOfFive());
+  }
+
+  /** @tests */
+  public function testIfNinetyIsMultipleOfBothThreeAndFive()
+  {
+    $linio = Linio::find(90);
+
+    $this->assertEquals($linio->number, 90);
+    $this->assertTrue($linio->isMultipleOfBoth());
+  }
+
+  /** @tests */
+  public function testIfThirtyOneIsNotMultipleOfThree()
+  {
+    $linio = Linio::find(31);
+
+    $this->assertEquals($linio->number, 31);
+    $this->assertFalse($linio->isMultipleOfThree());
+  }
+
+  /** @tests */
+  public function testIfFiftyOneIsMultipleOfFive()
+  {
+    $linio = Linio::find(51);
+
+    $this->assertEquals($linio->number, 51);
+    $this->assertFalse($linio->isMultipleOfFive());
+  }
+
+  /** @tests */
+  public function testIfNinetyOneIsNotMultipleOfBothThreeAndFive()
+  {
+    $linio = Linio::find(91);
+
+    $this->assertEquals($linio->number, 91);
+    $this->assertFalse($linio->isMultipleOfBoth());
+  }
 }
