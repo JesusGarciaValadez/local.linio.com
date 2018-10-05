6db44a0aeee01943b53d191523bc26bc94b45212
Jesús García <jesus.garciav@me.com>
2018-10-04 23:15:29 -0500

    Add database migration, model factory and table seeder for linios table

--- database/seeds/LinioTableSeeder.php
+++ database/seeds/LinioTableSeeder.php
@@ -4,13 +4,16 @@ use Illuminate\Database\Seeder;
 
 class LinioTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    foreach (range(1, 100) as $number)
     {
-        //
+      factory(App\Linio::class)->create(['number' => $number]);
     }
+  }
 }
