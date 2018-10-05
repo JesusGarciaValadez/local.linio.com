bfeb8aa7b7d000e29a6fd5825132248961f2d960
Jesús García <jesus.garciav@me.com>
2018-10-05 00:04:55 -0500

    Make a Linio Model with methods to check if a value is multiples of three or five.

--- app/Linio.php
+++ app/Linio.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Linio extends Model
 {
-    //
+  /**
+   * Check if the number attribute is multiple of three.
+   *
+   * @return boolean
+   */
+  public function isMultipleOfThree ()
+  {
+    return ($this->attributes['number'] % 3 === 0);
+  }
+
+  /**
+   * Check if the number attribute is multiples of five.
+   *
+   * @return boolean
+   */
+  public function isMultipleOfFive ()
+  {
+    return ($this->attributes['number'] % 5 === 0);
+  }
+
+  /**
+   * Check if the number attributes is both multiples of three and five.
+   *
+   * @return boolean
+   */
+  public function isMultipleOfBoth ()
+  {
+    return (
+      $this->attributes['number'] % 3 === 0 &&
+      $this->attributes['number'] % 5 === 0
+    );
+  }
 }
