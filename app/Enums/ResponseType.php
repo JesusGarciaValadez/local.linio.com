62c18db5f0325b71c2890396ca334610002ffc73
Jesús García <jesus.garciav@me.com>
2018-10-04 23:14:38 -0500

    Added enums for strings printable depends of the result.

---
+++ app/Enums/ResponseType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class ResponseType extends Enum
+{
+  const LINIO = 'Linio';
+  const IT = 'IT';
+  const LINIANOS = 'Linianos';
+}
